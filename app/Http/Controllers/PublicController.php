268762b9cda3848f83ec852036764eae7b76476d
Mqxxsy <gertmagi9@gmail.com>
2024-11-05 14:36:09 +0200

    display posts, pagination

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -7,8 +7,9 @@ use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
-    public function index() {
-        $posts = Post::all();
+    public function index()
+    {
+        $posts = Post::simplePaginate(16);
         return view('welcome', compact('posts'));
     }
 }
