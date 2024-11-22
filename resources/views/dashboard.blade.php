9bd1ee1b2c9be37c90da7ca64581a0aa44ff0688
Mqxsyy <gertmagi9@gmail.com>
2024-11-22 08:56:28 +0200

    redesign profile, dashboard and auth views

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,17 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('partials.layout')
+@section('title', 'Dashboard')
+@section('content')
+<div class="container max-w-7xl mx-auto">
+    <h2 class="font-semibold text-lg">
+        {{ __('Dashboard') }}
+    </h2>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
+    <div class="mt-6 card bg-base-300 shadow-xl">
+        <div class="card-body">
+            <div class="p-2">
+                You're logged in!
             </div>
         </div>
     </div>
-</x-app-layout>
+</div>
+@endsection
